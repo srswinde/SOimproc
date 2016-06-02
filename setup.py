a9414e43fd1c3f2a97de73871e86daccbcd812c0
scott <scottswindell@email.arizona.edu>
2016-06-02 16:42:28 -0700

    Added the setup.py file for installing.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='imgproc',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Scott Swindell',
+      author_email='scottswindell@email.arizona.edu',
+	  package_dir = {'':'src'},
+      py_modules = ['m4kproc'],
+     )
+
